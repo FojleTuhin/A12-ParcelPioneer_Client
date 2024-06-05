ebf49d519352a862e09068d6669305e9747ecc08
fojletuhin <fojletuhin@gmail.com>
2024-06-05 23:37:04 +0600

    add feature

---
+++ src/Pages/home/Features.jsx
@@ -0,0 +1,24 @@
+
+const Features = () => {
+    return (
+        <div className="flex flex-wrap justify-center gap-6">
+            <div>
+                <img src="https://i.ibb.co/7bst0N2/ic-Parcel-Feature-1.png" alt="" />
+                <p>On Demand Delivery</p>
+                <p>Left your charger or keys at home? Have it delivered to you on Pathao Parcel, without any hassle! </p>
+            </div>
+            <div>
+                <img src="https://i.ibb.co/7bst0N2/ic-Parcel-Feature-1.png" alt="" />
+                <p>On Demand Delivery</p>
+                <p>Left your charger or keys at home? Have it delivered to you on Pathao Parcel, without any hassle! </p>
+            </div>
+            <div>
+                <img src="https://i.ibb.co/7bst0N2/ic-Parcel-Feature-1.png" alt="" />
+                <p>On Demand Delivery</p>
+                <p>Left your charger or keys at home? Have it delivered to you on Pathao Parcel, without any hassle! </p>
+            </div>
+        </div>
+    );
+};
+
+export default Features;
