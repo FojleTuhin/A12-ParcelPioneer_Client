405013c6d222a250af787d3f01a64c186289b057
fojletuhin <fojletuhin@gmail.com>
2024-06-10 14:04:41 +0600

    fixed for only home page

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Shared/Navbar";
 import Footer from "@/Shared/Footer";
 
 const Root = () => {
+    const location = useLocation()
+    const isLogin = location.pathname.includes('login') || location.pathname.includes('signUp') 
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
-            <Footer></Footer>
+            {isLogin || <Footer></Footer>}
         </div>
     );
 };
