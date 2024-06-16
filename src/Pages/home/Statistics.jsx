cfa58b38cda231e3567d1769df7e99f8596b1fb2
fojletuhin <fojletuhin@gmail.com>
2024-06-16 16:49:19 +0600

    home page static component update

--- src/Pages/home/Statistics.jsx
+++ src/Pages/home/Statistics.jsx
@@ -1,25 +1,48 @@
+import useAxiosPublic from '@/hooks/useAxiosPublic';
+import useUsers from '@/hooks/useUsers';
+import { useQuery } from '@tanstack/react-query';
 import CountUp from 'react-countup';
 const Statistics = () => {
+
+    const axiosPublic = useAxiosPublic();
+    const [users] = useUsers();
+    
+    const { data: allParcel = [] } = useQuery({
+        queryKey: ['parcel'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/allParcel');
+            return res.data;
+        }
+    })
+    const { data: totalDelivered = [] } = useQuery({
+        queryKey: ['delivered'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/totalDelivered');
+            return res.data;
+        }
+    })
+
+    
     return (
         <div className="px-4 md:px-16 lg:px-[100px] flex flex-wrap justify-center gap-6  mt-12 mb-10">
             <div className="h-[150px] w-[300px] bg-[#EBFBE5] flex flex-col items-center justify-center rounded-2xl">
                 <p className="text-xl"> Parcel Booked</p>
                 <p className="font-semibold text-5xl">
-                    <CountUp end={100} duration={5}>
+                    <CountUp end={allParcel.length} duration={5}>
                     </CountUp>
                 </p>
             </div>
             <div className="h-[150px] w-[300px] bg-[#FAECFF] flex flex-col items-center justify-center rounded-2xl">
                 <p className="text-xl"> Parcel Delivered</p>
                 <p className="font-semibold text-5xl">
-                    <CountUp end={100} duration={5}>
+                    <CountUp end={totalDelivered.length} duration={5}>
                     </CountUp>
                 </p>
             </div>
             <div className="h-[150px] w-[300px] bg-[#EBFBE5] flex flex-col items-center justify-center rounded-2xl">
                 <p className="text-xl"> User</p>
                 <p className="font-semibold text-5xl">
-                    <CountUp end={100} duration={5}>
+                    <CountUp end={users.length} duration={5}>
                     </CountUp>
                 </p>
             </div>
