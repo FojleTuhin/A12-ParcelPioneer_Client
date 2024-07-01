5fcb54cf9613d2767a03a73b4c0bdf13e0955218
fojletuhin <fojletuhin@gmail.com>
2024-07-02 00:39:34 +0600

    update

--- src/Pages/home/Available.jsx
+++ src/Pages/home/Available.jsx
@@ -2,7 +2,7 @@
 const Available = () => {
     return (
         <div className="px-4 md:px-16 lg:px-[100px] mb-20 mt-28">
-            <div className='flex md:justify-between flex-col-reverse md:flex-row'>
+            <div className='flex justify-between flex-col-reverse md:flex-row'>
                 <div className='md:w-[48%] md:flex md:flex-col md:justify-center'>
                     <h1 className='font-bold text-4xl text-[#152934]'>Currently available in Dhaka and Chattogram</h1>
                     <p className='font-semibold text-xl text-[#5d6467] mt-10'>We are currently available in Dhaka and Chattogram. We are constantly expanding to more places! Parcel will cover more places soon.
