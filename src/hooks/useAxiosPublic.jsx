829cebf2d575753b6acef645e63de09c02ab0c5b
FojleTuhin <fojletuhin@gmail.com>
2025-04-30 03:35:48 +0600

    add api

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
-})
+  baseURL: "https://parcel-pioneer-server.vercel.app",
+});
 const useAxiosPublic = () => {
-    return axiosPublic;
+  return axiosPublic;
 };
 
 export default useAxiosPublic;
